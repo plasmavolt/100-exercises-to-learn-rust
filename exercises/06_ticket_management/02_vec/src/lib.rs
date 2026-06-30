fb7482bc624742a22b0c9c886a85c7a616112206
frank <frank@superscript.nyc>
2026-06-30 19:02:09 -0400

    06/00-06/02

--- exercises/06_ticket_management/02_vec/src/lib.rs
+++ exercises/06_ticket_management/02_vec/src/lib.rs
@@ -15,7 +15,11 @@ pub fn fibonacci(n: u32) -> u32 {
     //
     // Hint: use a `Vec` to memoize the results you have already calculated
     // so that you don't have to recalculate them several times.
-    todo!()
+    let mut nums = vec![0, 1];
+    for i in 2..=n {
+        nums.push(nums[(i - 1) as usize] + nums[(i - 2) as usize]);
+    }
+    nums[n as usize]
 }
 
 #[cfg(test)]
