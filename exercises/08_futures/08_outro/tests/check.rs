823b382d02dd9454bb5bb6e91f11bddb8da34268
frank <frank@superscript.nyc>
2026-07-21 10:38:18 -0400

    08/08 test: write e2e test

---
+++ exercises/08_futures/08_outro/tests/check.rs
@@ -0,0 +1,42 @@
+use axum_test::TestServer;
+use outro_08::*;
+use serde_json::json;
+
+fn create_test_server() -> TestServer {
+    TestServer::new(create_app())
+}
+
+#[tokio::test]
+async fn test_ticket() {
+    let server = create_test_server();
+
+    let create_response = server
+        .post(&"/create")
+        .json(&json!({
+            "title": "test",
+            "description": "lorem ipsum",
+        }))
+        .await;
+    let id = create_response.json::<outro_08::store::TicketId>();
+    create_response.assert_status_success();
+
+    let get_response = server
+        .get(&"/get")
+        .json(&serde_json::to_string(&id).unwrap())
+        .await;
+    get_response.assert_status_ok();
+    get_response.assert_json(&json!({
+        "title": "test",
+        "description": "lorem ipsum",
+    }));
+
+    let patch_response = server
+        .post(&"/update")
+        .json(&json!({
+            "id": id,
+            "title": "new title",
+            "status": "DONE",
+        }))
+        .await;
+    patch_response.assert_status_ok();
+}
